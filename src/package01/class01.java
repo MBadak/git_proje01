7fcdef762f015e54e5f9fe24d2aae249731deac0
Mbadak <m.badak99@gmail.com>
2020-06-21 21:33:19 +0200

    feature den ilk commit

--- src/package01/class01.java
+++ src/package01/class01.java
@@ -5,6 +5,7 @@ public class class01 {
 	public static void main(String[] args) {
 		System.out.println("ilk commit from local repository");
 		System.out.println("ikinci commit from local repository");
+		System.out.println("feature yaptiktan sonra commit from local repository");
 
 	}
 
