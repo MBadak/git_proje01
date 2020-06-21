b221d13b9f6b58716afe59712ccb86ccb60bb51c
Mbadak <m.badak99@gmail.com>
2020-06-21 21:14:18 +0200

    ikinci commit lokalden

--- src/package01/class01.java
+++ src/package01/class01.java
@@ -4,6 +4,7 @@ public class class01 {
 
 	public static void main(String[] args) {
 		System.out.println("ilk commit from local repository");
+		System.out.println("ikinci commit from local repository");
 
 	}
 
